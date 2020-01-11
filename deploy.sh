7dea75e941eb236dce9864163b1ca4bf447e2ca6
pengshiyu <1940607002@qq.com>
2020-01-12 00:24:37 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,24 @@
 
+# 编译
 yarn run build && \
 
+# 克隆
 git clone --depth 1 git@github.com:mouday/mouday.github.io.git site && \
 
+# 删除原有资源文件
+rm -rf ./site/css && \
+rm -rf ./site/fonts && \
+rm -rf ./site/js && \
+rm -rf ./site/img && \
+rm -f ./site/index.html && \
+
+# 拷贝
 cp -r ./dist/* ./site/ && cd site && \
 
-git add . && git commit -m "auto add" && git push
+# 添加push
+git add . && git commit -m "auto add" && git push && \
 
-cd .. && rm -rf site
+# 删除
+# cd .. && rm -rf site
 
 echo "发布完成"
