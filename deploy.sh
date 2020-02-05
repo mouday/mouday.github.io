b365e579aaf3ddce453fde0cb0dc2e1ab0349a46
pengshiyu <1940607002@qq.com>
2020-02-05 21:52:53 +0800

    auto add

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 yarn run build && \
 
 # 推page分支
-# git add . && git commit -m "auto add" && git push && \
+git add . && git commit -m "auto add" && git push && \
 
 # 克隆master分支
 # git clone --depth 1 git@github.com:mouday/mouday.github.io.git site && \
